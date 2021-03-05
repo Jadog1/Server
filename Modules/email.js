b3bfb36c56102241b74d4ed4da6a05867e2786f4
Jadog1 <steijr03@pfw.edu>
2021-03-05 15:43:34 -0500

    Refactored to make dynamic

--- Modules/email.js
+++ Modules/email.js
@@ -7,3 +7,24 @@ const transporter = nodemailer.createTransport({
         pass: 'xEv1Th1bgVPNnrJvPZ'
     }
 });
+
+let message = {
+    from: 'adelia.conn@ethereal.email',
+    to: 'steijr03@pfw.edu',
+    subject: 'Nodemailer is unicode friendly ?',
+    text: 'Hello to myself!',
+    html: '<p><b>Hello</b> to myself!</p>'
+};
+
+exports.sendEmail = () => {
+    transporter.sendMail(message, (err, info) => {
+        if (err) {
+            console.log('Error occurred. ' + err.message);
+            return process.exit(1);
+        }
+
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+}
--- server.js
+++ server.js
@@ -12,43 +12,38 @@ app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
+app.listen(port);
+
 // set the home page route
 app.get('/', function (req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
+    res.render('pages/Home');
+});
+app.get('/game', function (req, res) {
+    res.render('pages/SeniorGame');
+});
+app.get('/distort', function (req, res) {
+    res.render('pages/ImageDistort');
+});
+app.get('/distance', function (req, res) {
+    res.render('pages/Distance');
+});
+app.get('/resume', function (req, res) {
+    fsExtend.readExtend('Files/Resume.pdf', 'text/pdf', res);
+});
+app.get('/engagement', function (req, res) {
+    fsExtend.readExtend('Files/ProfilePic.jpg', 'image/jpeg', res);
 });
 
-http.createServer(function (req, res) {
-    switch (req.url) {
-        case '/': 
-            fsExtend.readExtend('views/Home.html', 'text/html', res);
-            break;
-        case '/game':
-            fsExtend.readExtend('views/SeniorGame.html', 'text/html', res);
-            break;
-        case '/distort':
-            fsExtend.readExtend('views/ImageDistort.html', 'text/html', res);
-            break;
-        case '/distance':
-            fsExtend.readExtend('views/Distance.html', 'text/html', res);
-            break;
-        case '/resume':
-            fsExtend.readExtend('Files/Resume.pdf', 'text/pdf', res);
-            break;
-        case '/engagement':
-            fsExtend.readExtend('Files/ProfilePic.jpg', 'image/jpeg', res);
-            break;
-        default:
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.write("<h1>No page found!</h1>");
-            res.end();
-            break;
-    }
-}).listen(port);
 
 process.on('SIGTERM', () => {
     server.close(() => {
-        console.log('Process terminated')
+        console.log('Process terminated');
     })
-})
+});
+
+process.on('uncaughtException', err => {
+    console.error('There was an uncaught error', err)
+    process.exit(1) //mandatory (as per the Node.js docs)
+});
+
+
